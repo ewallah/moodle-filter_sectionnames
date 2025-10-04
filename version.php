a922dabf3423918a815ad948e3607628f1131eab
Renaat Debleu <rdebleu@eWallah.net>
2025-10-04 11:39:14 +0000

    501

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->component = 'filter_sectionnames';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v5.0.1';
 $plugin->version = 2025082200;
-$plugin->requires = 2024100700;
-$plugin->supported = [405, 500];
+$plugin->requires = 2025041400;
+$plugin->supported = [500, 501];
